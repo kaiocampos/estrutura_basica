ca98996826efdc8361f777f2e9a976c635e1ca1a
Kaio Campos <kaiocamposti@gmail.com>
2020-01-23 18:46:26 -0300

    estrutura basica

---
+++ app/config.php
@@ -0,0 +1,27 @@
+<?php
+require 'enviroment.php';
+
+$config = array();
+
+if (ENVIROMENT == 'development') {
+    define("BASE_URL", "http://localhost/estrutura_basica/app");
+    $config['dbname'] = 'estrutura_mvc';
+    $config['host'] = 'localhost';
+    $config['dbuser'] = 'root';
+    $config['dbpass'] = '';
+}else{
+    define("BASE_URL", "http://www.meusite.com.br");
+    $config['dbname'] = 'estrutura_mvc';
+    $config['host'] = 'localhost';
+    $config['dbuser'] = 'root';
+    $config['dbpass'] = '';
+}
+global $db;
+try {
+    $db = new PDO("mysql:dbname=".$config['dbname'].";host=".$config['host'], $config['dbuser'],$config['dbpass']);
+   
+
+} catch (Throwable $th) {
+    throw $th;
+    exit;
+}
